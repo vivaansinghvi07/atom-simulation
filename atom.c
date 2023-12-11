64b08cc26c7ef5d4bb6eee3432a3f1faf548aec4
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2023-12-10 21:31:22 -0500

    forgot to commit, some abstraction of functions

--- atom.c
+++ atom.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 // fine tune these parameters to control gravitation 
-#define GRAVITATIONAL_CONSTANT 0.0005
+#define GRAVITATIONAL_CONSTANT 0.5
 #define GRAVITATIONAL_DISTANCE_GUARD 20000
 
 typedef struct point { 
@@ -18,18 +18,25 @@ typedef struct atom {
         Point acceleration;
 } Atom;
 
+double abs_point(Point *p);
 Atom *init_atoms(int n_atoms, int x_max, int y_max);
 void debug_atoms(Atom *atoms, int n_atoms);
 void apply_gravity_to_center(Atom *atoms, int n_atoms);
 void apply_gravity_each_point_naive(Atom *atoms, int n_atoms);
+Point get_gravity_of_a(int mass_a, Point *com_a, Point *com_b);
 void step_simulation(Atom *atoms, int n_atoms);
+
+double abs_point(Point *p) {
+        return sqrt(p->x * p->x + p->y * p->y);
+}
 
 Atom *init_atoms(int n_atoms, int x_max, int y_max) {
         srand(100);
         Atom *atoms = malloc(sizeof(Atom) * n_atoms);
         for (int i = 0; i < n_atoms; ++i) {
-                atoms[i].mass = 1; 
-                atoms[i].position = (Point) {rand() % x_max, rand() % y_max};
+                        atoms[i].mass = 1; 
+                atoms[i].position = (Point) {.x = rand() % (x_max / 2) + (x_max / 4), 
+                                             .y = rand() % (y_max / 2) + (y_max / 4) };
         }
         return atoms;
 }
@@ -56,11 +63,6 @@ void step_simulation(Atom *atoms, int n_atoms) {
         }
 }
 
-// google "pauli repulsive force"
-void apply_repulsive_force(Atom *atoms, int n_atoms) {
-
-}
-
 // calculates the gravity of each point by considering the gravity of each other point - O(n^2) always
 void apply_gravity_each_point_naive(Atom *atoms, int n_atoms) {
         for (int i = 0; i < n_atoms; ++i) {
@@ -70,12 +72,9 @@ void apply_gravity_each_point_naive(Atom *atoms, int n_atoms) {
                                 continue;
                         }
                         Atom *other = &atoms[j];
-                        Point distance = {.x = other->position.x - atom->position.x, .y = other ->position.y - atom->position.y};
-                        double angle_to_other = atan2(distance.y, distance.x);
-                        double overall_distance = sqrt(distance.y * distance.y + distance.x * distance.x);
-                        double acceleration_gravity = other->mass * GRAVITATIONAL_CONSTANT / (overall_distance * overall_distance + GRAVITATIONAL_DISTANCE_GUARD);
-                        atom->acceleration.x += acceleration_gravity * cos(angle_to_other);
-                        atom->acceleration.y += acceleration_gravity * sin(angle_to_other);
+                        Point acceleration_gravity = get_gravity_of_a(other->mass, &other->position, &atom->position);
+                        atom->acceleration.x += acceleration_gravity.x;
+                        atom->acceleration.y += acceleration_gravity.y;
                 }
         }
 }
@@ -95,18 +94,29 @@ void apply_gravity_to_center(Atom *atoms, int n_atoms) {
         center_of_mass.x /= total_mass;
         center_of_mass.y /= total_mass;
         
-        // g = \frac{G * m_i}{r_i^2} // in this case, a constant is being added
         // to find the distance, first undo the center of mass formula to adjust for the "missing" point 
-        // then, use that new center of mass to calculate 
+        // then, use that new center of mass to calculate gravitational acceleration
         for (int i = 0; i < n_atoms; ++i) {
                 Atom *atom = &atoms[i];
                 double adjusted_com_x = (center_of_mass.x * total_mass - atom->position.x * atom->mass) / (total_mass - atom->mass);
                 double adjusted_com_y = (center_of_mass.y * total_mass - atom->position.y * atom->mass) / (total_mass - atom->mass);
-                Point distance = {.x = adjusted_com_x - atom->position.x, .y = adjusted_com_y - atom->position.y};
-                double angle_to_com = atan2(distance.y, distance.x);
-                double overall_distance = sqrt(distance.y * distance.y + distance.x * distance.x);
-                double acceleration_gravity = (total_mass - atom->mass) * GRAVITATIONAL_CONSTANT / (overall_distance * overall_distance + GRAVITATIONAL_DISTANCE_GUARD);
-                atom->acceleration.x = acceleration_gravity * cos(angle_to_com);
-                atom->acceleration.y = acceleration_gravity * sin(angle_to_com);
+
+                Point acceleration_gravity = get_gravity_of_a(total_mass - atom->mass, 
+                                                              &(Point){.x = adjusted_com_x, .y = adjusted_com_y}, 
+                                                              &atom->position);
+                atom->acceleration.x = acceleration_gravity.x;
+                atom->acceleration.y = acceleration_gravity.y;
         }
+}
+
+// apply the following: 
+// g = \frac{G * m_i}{r_i^2}
+// where r_i is the distance between com_a and com_b
+Point get_gravity_of_a(int mass_a, Point *com_a, Point *com_b) {
+        
+        Point distance = {.x = com_a->x - com_b->x, .y = com_a->y - com_b->y};
+        double angle_to_a = atan2(distance.y, distance.x); 
+        double overall_distance = sqrt(distance.y * distance.y + distance.x * distance.x);
+        double acceleration_gravity = mass_a * GRAVITATIONAL_CONSTANT / (overall_distance * overall_distance + GRAVITATIONAL_DISTANCE_GUARD);
+        return (Point) { .x = acceleration_gravity * cos(angle_to_a), .y = acceleration_gravity * sin(angle_to_a) };
 }
